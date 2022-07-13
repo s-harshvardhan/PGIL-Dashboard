26a93a4b52cf0aff12d31c6c85c1c564f26601b8
s-harshvardhan <90432351+s-harshvardhan@users.noreply.github.com>
2022-07-13 15:07:48 +0530

    Add files via upload

---
+++ styles.js
@@ -0,0 +1,7 @@
+document.querySelector(".add-style-card").addEventListener("click", function() {
+    alert("You need to be logged in to add a new style!");
+} )
+
+document.querySelector("#raiseConcernButton").addEventListener("click", function() {
+    alert("Your request has been submitted successfully!")
+}) 
