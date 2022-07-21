7e974342f15276c5cba79a61aa42af7855311c1f
s-harshvardhan <90432351+s-harshvardhan@users.noreply.github.com>
2022-07-21 17:30:45 +0530

    Delete styles.js

--- styles.js
+++
@@ -1,7 +0,0 @@
-document.querySelector(".add-style-card").addEventListener("click", function() {
-    alert("You need to be logged in to add a new style!");
-} )
-
-document.querySelector("#raiseConcernButton").addEventListener("click", function() {
-    alert("Your request has been submitted successfully!")
-}) 
